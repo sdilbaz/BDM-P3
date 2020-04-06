43b34ae19bcfaa8036280ca50f9c37cbdd17e4af
Hamid Sakhi <hamidsakhi@Hamids-MacBook-Pro-2.local>
2020-04-06 06:26:47 -0400

    T1, T2, T3, and T4 is done working on T5 and T6

--- Problem 1/datasetsgeneration.py
+++ Problem 1/datasetsgeneration.py
@@ -3,7 +3,7 @@ import string
 import csv
 
 def employeegeneration(): 
-    with open('employee.csv', mode = 'w') as emp: 
+    with open('customer.csv', mode = 'w') as emp: 
         csvwrite = csv.writer(emp)
         for id in range(1,50001):
             stringrange = string.ascii_letters
@@ -15,10 +15,10 @@ def employeegeneration():
             csvwrite.writerow(employee)
 
 def transactiongeneration():
-    with open('transaction.csv', mode ='w') as trx:
+    with open('purchase.csv', mode ='w') as trx:
         csvwrite = csv.writer(trx)
         for transid in range(1,5000001):
-            custid = random.randint(1,50000)
+            custid = random.randint(1,50001)
             transtotal = round(random.uniform(10.0, 2000.0),2)
             transnumitem = random.randint(1,15)
             stringrange = string.ascii_letters
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # BDM-P3
 Project 3 for CS585/DS503: Big Data Management
-
-## Problem 1 (Hamidullah Sakhi)
-### Data Generation
+##Serdarcan Dilbaz and Hamidullah Sakhi
+### Problem 1 (Hamidullah Sakhi)
+#### Data Generation
 Simple python code for generating the two datasets
