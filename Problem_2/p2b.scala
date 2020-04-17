53bba9aa069aad001963309f5f355d85f89fb230
sdilbaz <serdarcan.dilbaz@gmail.com>
2020-04-17 11:02:49 +0300

    Update p2b.scala

--- Problem_2/p2b.scala
+++ Problem_2/p2b.scala
@@ -1,16 +1,12 @@
 import scala.collection.mutable.ListBuffer
 
-val grid_size=9
-
-def list2id(tup:List[Int]): Int = {
-	return tup(0) + tup(1)*grid_size+1
-}
+val grid_size=500
 
 def get_neighbors(tup: List[Int]): List[List[Int]] ={
 	val all=List(List(tup(0)-1,tup(1)-1),List(tup(0)-1,tup(1)),List(tup(0)-1,tup(1)+1),
 	List(tup(0),tup(1)-1),List(tup(0),tup(1)),List(tup(0),tup(1)+1),
 	List(tup(0)+1,tup(1)-1),List(tup(0)+1,tup(1)),List(tup(0)+1,tup(1)+1))
-	return valid=all.filter(x => x(0)>=0 && x(1)>=0 && x(0)<grid_size && x(1)< grid_size)
+	return all.filter(x => x(0)>=0 && x(1)>=0 && x(0)<grid_size && x(1)< grid_size)
 }
 
 def enrich(tt: (List[Int], Int)): List[(List[Int], Int)] = {
@@ -25,8 +21,11 @@ def append = (l1: ListBuffer[Double], value: Int) =>
 
 def extend = (l1: ListBuffer[Double], l2: ListBuffer[Double]) =>
 	(l1++l2)
-    
 
+def list2id(tup:List[Int]): Int = {
+	return tup(0) + tup(1)*grid_size+1
+}
+    
 val file_name="points_k_means.txt"
 val raw_data=sc.textFile(file_name)
 val data=raw_data.map(x => (x.substring(1,x.length()-1).split(","))).map(x => Vector(x(0).toInt,x(1).toInt))
@@ -39,3 +38,5 @@ val base=enriched.combineByKey(to_list, append, extend)
 val template = counts.join(base)
 val rd_scores=template.map(x=>(list2id(x._1),x._2._1/x._2._2.sum*x._2._2.length)).sortBy(_._2,false)
 
+println("Results:")
+rd_scores.take(16)
