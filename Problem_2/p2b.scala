dafaf35f70057822f219d5d30554cc89630f59dd
sdilbaz <serdarcan.dilbaz@gmail.com>
2020-04-21 08:23:56 +0300

    Add files via upload

--- Problem_2/p2b.scala
+++ Problem_2/p2b.scala
@@ -1,15 +1,17 @@
+//26 seconds
+
 import scala.collection.mutable.ListBuffer
 
 val grid_size=500
 
-def get_neighbors(tup: List[Int]): List[List[Int]] ={
-	val all=List(List(tup(0)-1,tup(1)-1),List(tup(0)-1,tup(1)),List(tup(0)-1,tup(1)+1),
-	List(tup(0),tup(1)-1),List(tup(0),tup(1)),List(tup(0),tup(1)+1),
-	List(tup(0)+1,tup(1)-1),List(tup(0)+1,tup(1)),List(tup(0)+1,tup(1)+1))
-	return all.filter(x => x(0)>=0 && x(1)>=0 && x(0)<grid_size && x(1)< grid_size)
+def get_neighbors(tup: (Int, Int)): List[(Int, Int)] ={
+	val all=List((tup._1-1,tup._2-1),(tup._1-1,tup._2),(tup._1-1,tup._2+1),
+	(tup._1,tup._2-1),(tup._1,tup._2),(tup._1,tup._2+1),
+	(tup._1+1,tup._2-1),(tup._1+1,tup._2),(tup._1+1,tup._2+1))
+	return all.filter(x => x._1>=0 && x._2>=0 && x._1<grid_size && x._2< grid_size)
 }
 
-def enrich(tt: (List[Int], Int)): List[(List[Int], Int)] = {
+def enrich(tt: ((Int, Int), Int)): List[((Int, Int), Int)] = {
 	return get_neighbors(tt._1).map(a=>(a,tt._2))
 }
 
@@ -22,21 +24,21 @@ def append = (l1: ListBuffer[Double], value: Int) =>
 def extend = (l1: ListBuffer[Double], l2: ListBuffer[Double]) =>
 	(l1++l2)
 
-def list2id(tup:List[Int]): Int = {
-	return tup(0) + tup(1)*grid_size+1
+def tuple2int(x: (Int,Int)): Int = {
+	return (x._1+x._2*grid_size+1)
 }
-    
+
 val file_name="points_k_means.txt"
 val raw_data=sc.textFile(file_name)
-val data=raw_data.map(x => (x.substring(1,x.length()-1).split(","))).map(x => Vector(x(0).toInt,x(1).toInt))
-val regionIds = data.map(x => (List((x(0)-1)/20,(x(1)-1)/20),1))
+val data=raw_data.map(x => (x.substring(1,x.length()-1).split(","))).map(x => (x(0).toInt,x(1).toInt))
+val regionIds = data.map(x => (((x._1-1)/20,(x._2-1)/20),1))
 
 val counts=regionIds.reduceByKey((a,b)=>a+b)
 val enriched=counts.flatMap(enrich)
 val base=enriched.combineByKey(to_list, append, extend)
 
 val template = counts.join(base)
-val rd_scores=template.map(x=>(list2id(x._1),x._2._1/x._2._2.sum*x._2._2.length)).sortBy(_._2,false)
+val rd_scores=template.map(x=>(tuple2int(x._1),x._2._1/x._2._2.sum*x._2._2.length)).sortBy(_._2,false)
 
 println("Results:")
 println(rd_scores.take(16).deep.mkString("\n"))
--- Problem_2/p2c.scala
+++ Problem_2/p2c.scala
@@ -1,15 +1,17 @@
+//26 seconds
+
 import scala.collection.mutable.ListBuffer
 
 val grid_size=500
 
-def get_neighbors(tup: List[Int]): List[List[Int]] ={
-	val all=List(List(tup(0)-1,tup(1)-1),List(tup(0)-1,tup(1)),List(tup(0)-1,tup(1)+1),
-	List(tup(0),tup(1)-1),List(tup(0),tup(1)),List(tup(0),tup(1)+1),
-	List(tup(0)+1,tup(1)-1),List(tup(0)+1,tup(1)),List(tup(0)+1,tup(1)+1))
-	return all.filter(x => x(0)>=0 && x(1)>=0 && x(0)<grid_size && x(1)< grid_size)
+def get_neighbors(tup: (Int, Int)): List[(Int, Int)] ={
+	val all=List((tup._1-1,tup._2-1),(tup._1-1,tup._2),(tup._1-1,tup._2+1),
+	(tup._1,tup._2-1),(tup._1,tup._2),(tup._1,tup._2+1),
+	(tup._1+1,tup._2-1),(tup._1+1,tup._2),(tup._1+1,tup._2+1))
+	return all.filter(x => x._1>=0 && x._2>=0 && x._1<grid_size && x._2< grid_size)
 }
 
-def enrich(tt: (List[Int], Int)): List[(List[Int], Int)] = {
+def enrich(tt: ((Int, Int), Int)): List[((Int, Int), Int)] = {
 	return get_neighbors(tt._1).map(a=>(a,tt._2))
 }
 
@@ -22,34 +24,34 @@ def append = (l1: ListBuffer[Double], value: Int) =>
 def extend = (l1: ListBuffer[Double], l2: ListBuffer[Double]) =>
 	(l1++l2)
 
-def list2id(tup:List[Int]): Int = {
-	return tup(0) + tup(1)*grid_size+1
+def tuple2int(x: (Int,Int)): Int = {
+	return (x._1+x._2*grid_size+1)
 }
 
-def id2list(id: Int): List[Int] ={
-    return List(java.lang.Math.floorMod(id-1,grid_size),java.lang.Math.floorDiv(id-1,grid_size))
+def id2tuple(id: Int): (Int,Int) ={
+	return (java.lang.Math.floorMod(id-1,grid_size),java.lang.Math.floorDiv(id-1,grid_size))
 }
 
 val file_name="points_k_means.txt"
 val raw_data=sc.textFile(file_name)
-val data=raw_data.map(x => (x.substring(1,x.length()-1).split(","))).map(x => Vector(x(0).toInt,x(1).toInt))
-val regionIds = data.map(x => (List((x(0)-1)/20,(x(1)-1)/20),1))
+val data=raw_data.map(x => (x.substring(1,x.length()-1).split(","))).map(x => (x(0).toInt,x(1).toInt))
+val regionIds = data.map(x => (((x._1-1)/20,(x._2-1)/20),1))
 
 val counts=regionIds.reduceByKey((a,b)=>a+b)
 val enriched=counts.flatMap(enrich)
 val base=enriched.combineByKey(to_list, append, extend)
 
 val template = counts.join(base)
-val rd_scores=template.map(x=>(list2id(x._1),x._2._1/x._2._2.sum*x._2._2.length)).sortBy(_._2,false)
+val rd_scores=template.map(x=>(tuple2int(x._1),x._2._1/x._2._2.sum*x._2._2.length)).sortBy(_._2,false)
 val sorted_scores = rd_scores.sortByKey()
 
 def add_data(line:(Int, Double)): (Int, (Double, Int, List[(Int, Double)])) = {
-    val ns = get_neighbors(id2list(line._1))
-    val aux = ns.map(a => (list2id(a),sorted_scores.lookup(list2id(a))(0)))
+    val ns = get_neighbors(id2tuple(line._1))
+    val aux = ns.map(a => (tuple2int(a),sorted_scores.lookup(tuple2int(a))(0)))
     return (line._1,(line._2,ns.length,aux))
+}
 
 val results=rd_scores.take(16).map(add_data)
 println("Results:")
 println(results.deep.mkString("\n"))
-
 
